951b413e65e5a7efdb4d168654aa47c404bfa1db
Christopher McCloud <mccloud.christopher@gmail.com>
2017-05-20 21:37:52 -0500

    sets utf-8 as default encoding

--- init.el
+++ init.el
@@ -60,7 +60,16 @@
   (use-package hlinum :demand t)
   (hlinum-activate))
 
-;;; ### FROM DOOM UI CORE ### 
+;;; ### FROM DOOM UI CORE  AND CORE###
+(when (fboundp 'set-charset-priority)
+  (set-charset-priority 'unicode))     
+(prefer-coding-system        'utf-8)   
+(set-terminal-coding-system  'utf-8)   
+(set-keyboard-coding-system  'utf-8)   
+(set-selection-coding-system 'utf-8)   
+(setq locale-coding-system   'utf-8)   
+(setq-default buffer-file-coding-system 'utf-8)
+
 (setq-default frame-title-format nil
               bidi-display-reordering nil
               blink-matching-paren nil
