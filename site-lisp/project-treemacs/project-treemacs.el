389a5417217a5e1c1a96a9963efc00eed0b053c6
Christopher McCloud <mccloud.christopher@gmail.com>
2021-03-16 20:41:24 -0500

    Adds basic file caching

--- site-lisp/project-treemacs/project-treemacs.el
+++ site-lisp/project-treemacs/project-treemacs.el
@@ -36,9 +36,39 @@
 
 ;;; Code:
 (require 'seq)
+(require 'ht)
 (require 'treemacs)
 (require 'project)
 
+;; TODO: Implement project-ignores
+
+(defgroup project-treemacs nil
+  "Support for treemacs based projects."
+  :group 'project)
+
+(defcustom project-treemacs-ignores nil
+  "List of patterns to add to `project-ignores'."
+  :type '(repeat string)
+  :safe #'listp)
+
+(defvar project-treemacs--files-cache (ht-create)
+  "Stores project-treemacs `project-files'.
+Only used when `treemacs-filewatch-mode' is enabled.")
+
+(defun project-treemacs--clear-cache ()
+  (setq project-treemacs--files-cache (ht-create)))
+
+(defun project-treemacs--get-files-for-dir (dir)
+  (or (and treemacs-filewatch-mode (ht-get project-treemacs--files-cache dir))
+      (progn (ht-set project-treemacs--files-cache dir
+		      (directory-files-recursively dir ".*" nil t nil))
+	     (ht-get project-treemacs--files-cache dir))))
+
+(defun project-treemacs--files (project &optional dirs)
+  (seq-mapcat (lambda (d) (project-treemacs--get-files-for-dir d))
+	      (append (list (project-root project)) dirs)))
+
+;; Project.el API
 (defun project-treemacs-try (dir)
   (treemacs--find-project-for-path dir))
 
@@ -46,8 +76,10 @@
   (treemacs-project->path project))
 
 (cl-defmethod project-files ((project treemacs-project) &optional dirs)
-  (seq-mapcat (lambda (d) (directory-files-recursively d ".*" nil t nil))
-	      (append (list (project-root project)) dirs)))
+  (benchmark-progn (project-treemacs--files project dirs)))
+
+(cl-defmethod project-ignores ((project treemacs-project))
+  project-treemacs-ignores)
 
 (cl-defmethod project-external-roots ((project treemacs-project))
   (remove (project-root project)
@@ -55,6 +87,14 @@
 		  (treemacs-workspace->projects
 		   (treemacs-current-workspace)))))
 
+;; Cache invalidation
+(define-advice treemacs--process-file-events
+    (:after (&rest r) update-cache)
+  (project-treemacs--clear-cache))
+
+(add-hook 'treemacs-switch-workspace-hook #'project-treemacs--clear-cache)
+
+;; Enable project-treemacs, and prefer it to project-vc
 (add-to-list 'project-find-functions #'project-treemacs-try)
 
 (provide 'project-treemacs)
