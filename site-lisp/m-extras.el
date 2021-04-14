7298b7661ca60bf9b9265e15969e00768d61af40
Christopher McCloud <mccloud.christopher@gmail.com>
2021-04-14 18:49:08 -0500

    adds copyright

--- site-lisp/m-extras.el
+++ site-lisp/m-extras.el
@@ -1,10 +1,27 @@
 ;;; m-extras.el --- Personal extensions for emacs packages -*- lexical-binding: t -*-
 
-;; Copyright (C) 2020 Christopher McCloud
+;; Copyright (C) 2020-2021 Christopher McCloud
 
 ;; Author: Christopher McCloud <mccloud.christopher@gmail.com>
 
 ;; This file is not part of GNU Emacs
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;;; Code:
 (require 'cl-seq)
