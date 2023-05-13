4623b050c7c8a808bbc850f6fccb17e8c3a8aa56
Christopher McCloud <mccloud.christopher@gmail.com>
2023-05-13 08:32:49 -0500

    Adds GNU Devel Archive

--- early-init.el
+++ early-init.el
@@ -52,6 +52,7 @@
 		     ("ublt" . "https://elpa.ubolonton.org/packages/")
                      ("org" . "http://orgmode.org/elpa/")
                      ("gnu" . "http://elpa.gnu.org/packages/")
+                     ("gnu-devel" . "http://elpa.gnu.org/devel/")
 		     ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 (customize-set-variable
  'package-archive-priorities '(("melpa" . 10)
